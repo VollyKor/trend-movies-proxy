6c275d4a6acc22c844db62e4f5dd102ed4b57d31
VolKor <vol.kor.93.@gamil.com>
2021-06-28 17:00:48 +0300

    update db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-
 import { StatisticModule } from './statistic/statistic.module';
-
 @Module({
     imports: [
+        ConfigModule.forRoot(),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5450,
-            username: 'postgres',
-            password: 'Respubli4ka',
-            database: 'trend_movies',
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT) || 5432,
+            username: process.env.DB_USER_NAME || 'postgres',
+            password: process.env.DB_PASSWORD || '',
+            database: process.env.DB_DATABASE || 'postgres',
             autoLoadModels: true,
             synchronize: true,
         }),
